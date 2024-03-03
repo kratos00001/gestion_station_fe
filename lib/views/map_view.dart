a630fd6a6e98242a59753a36356f6f92b6ec955a
kratos00001 <yassine.bouhsaien@etu.uae.ac.ma>
2024-03-03 10:22:41 +0000

    styling upgrades

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-import '../services/api_service.dart';
+import 'package:geolocator/geolocator.dart';
+import '../models/station.dart'; // Make sure this model has all necessary fields
+import '../services/api_service.dart'; // Your API service
+import 'station_details_page.dart'; // Import the StationDetailsPage
 
 class MapView extends StatefulWidget {
   @override
@@ -22,26 +23,52 @@ class _MapViewState extends State<MapView> {
   Future<void> _loadStations() async {
     var stations = await ApiService().fetchStations();
     setState(() {
-      _markers = stations
-          .map((station) => Marker(
+      _markers = stations.map((station) => Marker(
         markerId: MarkerId(station.id.toString()),
         position: LatLng(station.latitude, station.longitude),
         infoWindow: InfoWindow(
           title: station.nom,
           snippet: station.adresse,
+          // When the infoWindow is tapped, navigate to the details page
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => StationDetailsPage(station: station),
+            ));
+          },
         ),
-        // icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet), // Example for custom marker color
-      ))
-          .toSet();
+      )).toSet();
     });
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    LatLng currentLocation = LatLng(position.latitude, position.longitude);
+    setState(() {
+      _markers.add(
+        Marker(
+          // Provide a unique ID for the current location marker
+          markerId: MarkerId("current_location"),
+          position: currentLocation,
+          infoWindow: InfoWindow(
+            title: 'My Location',
+          ),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure), // Custom color
+        ),
+      );
+    });
+    mapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: currentLocation,
+        zoom: 14.0,
+      ),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Stations Map', style: TextStyle(color: Colors.white)), // Adjust the title style if needed
-        backgroundColor: Colors.deepPurple, // Adjust AppBar color to fit your theme
+        title: Text('Stations Map'),
       ),
       body: GoogleMap(
         onMapCreated: _onMapCreated,
@@ -50,11 +77,12 @@ class _MapViewState extends State<MapView> {
           zoom: 11.0,
         ),
         markers: _markers,
+        zoomControlsEnabled: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _loadStations(), // Example action: Refresh stations
-        child: Icon(Icons.refresh),
-        backgroundColor: Colors.deepPurple, // Adjust FAB color to fit your theme
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToCurrentLocation,
+        label: Text('My Location'),
+        icon: Icon(Icons.location_searching),
       ),
     );
   }
